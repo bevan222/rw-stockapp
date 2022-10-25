1c716f5e7a101bcceb8f360273ff3b3310e1e289
bevan222 <bevan2628@gmail.com>
2022-10-25 21:45:48 +0800

    add footer content

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
         <footer className = "navbar-fixed-bottom bg-dark text-light py-2">
             <div className="container text-center">
                 &copy; {today.getFullYear()} <a href="https://rw-stockapp.web.app/" class="">rw-stockapp</a>
-                <span className="small text-muted">{process.env.REACT_APP_FUGLE_VERSION}</span>
                 <br />
-                {/*<a href="https://BizMoney.tw/contact_us" class="">聯絡我們</a>
-                |*/}
-                Powered By <a href="https://github.com/bevan222" target="_blank" className="" rel="noopener">Bevan</a>            
+                <span className="small text-muted">V{process.env.REACT_APP_VERSION}  </span>
+                Powered By <a href="https://github.com/bevan222" target="_blank" className="" rel="noopener">Bevan</a>
+                &nbsp;
+                <a href = "mailto: bevan2628@gmail.com">聯絡我</a>            
             </div>
         </footer>
     )

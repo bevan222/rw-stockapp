9b71aa3d948c58bf4593cf2c62e364ec336a4d40
bevan222 <bevan2628@gmail.com>
2022-11-18 23:47:36 +0800

    mod: modify navbar dropdown item's marging

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -27,19 +27,19 @@ const NavBar = () => {
                             menuVariant="dark"
                         >
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.twse.com.tw/zh/holidaySchedule/holidaySchedule">市場開休市</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.twse.com.tw/zh/holidaySchedule/holidaySchedule">市場開休市</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.twse.com.tw/zh/page/trading/exchange/TWTAVU.html">減資預告表</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.twse.com.tw/zh/page/trading/exchange/TWTAVU.html">減資預告表</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.twse.com.tw/zh/page/announcement/notice.html">注意股</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.twse.com.tw/zh/page/announcement/notice.html">注意股</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.twse.com.tw/zh/page/announcement/punish.html">處置股</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.twse.com.tw/zh/page/announcement/punish.html">處置股</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.twse.com.tw/zh/page/trading/exchange/TWT48U.html">除權息</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.twse.com.tw/zh/page/trading/exchange/TWT48U.html">除權息</a>
                             </li>
                         </NavDropdown>
                         <NavDropdown
@@ -47,19 +47,19 @@ const NavBar = () => {
                             menuVariant="dark"
                         >
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.tpex.org.tw/web/bulletin/trading_date/trading_date.php?l=zh-tw">市場開休市</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.tpex.org.tw/web/bulletin/trading_date/trading_date.php?l=zh-tw">市場開休市</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.tpex.org.tw/web/stock/exright/decap/decap.php?l=zh-tw">減資預告表</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.tpex.org.tw/web/stock/exright/decap/decap.php?l=zh-tw">減資預告表</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.tpex.org.tw/web/bulletin/attention_information/trading_attention_information.php?l=zh-tw">注意股</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.tpex.org.tw/web/bulletin/attention_information/trading_attention_information.php?l=zh-tw">注意股</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.tpex.org.tw/web/bulletin/disposal_information/disposal_information.php?l=zh-tw">處置股</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.tpex.org.tw/web/bulletin/disposal_information/disposal_information.php?l=zh-tw">處置股</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" target="_blank" href="https://www.tpex.org.tw/web/stock/exright/preAnnounce/PrePost.php?l=zh-tw">除權息</a>
+                                <a className="nav-link mx-1" target="_blank" href="https://www.tpex.org.tw/web/stock/exright/preAnnounce/PrePost.php?l=zh-tw">除權息</a>
                             </li>
                         </NavDropdown>
                     </ul>
